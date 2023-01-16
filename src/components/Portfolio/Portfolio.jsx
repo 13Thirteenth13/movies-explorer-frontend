41bab8e5d8543780d8af17a2db9a052d0d34b18b
Sergey Krivenko <Krivenko.Sergei.V@gmail.com>
2023-01-16 21:48:55 +0300

    feat: Portfolio; fix: links hover #1

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,46 @@
+import arrow from "../../images/arrow.svg";
+
+function Portfolio() {
+  return (
+    <section className="portfolio">
+      <p className="portfolio__title">Портфолио</p>
+      <ul className="portfolio__list">
+        <li className="portfolio__list-item portfolio__underline">
+          <a
+            className="portfolio__link"
+            href="https://github.com/13Thirteenth13/how-to-learn"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p className="portfolio__link-text">Статичный сайт</p>
+            <img src={arrow} alt="Иконка стрелка" />
+          </a>
+        </li>
+        <li className="portfolio__list-item portfolio__underline">
+          <a
+            className="portfolio__link"
+            href="https://github.com/13Thirteenth13/russian-travel"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p className="portfolio__link-text">Адаптивный сайт</p>
+            <img src={arrow} alt="Иконка стрелка" />
+          </a>
+        </li>
+        <li className="portfolio__list-item">
+          <a
+            className="portfolio__link"
+            href="https://github.com/13Thirteenth13/mesto/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p className="portfolio__link-text">Одностраничное приложение</p>
+            <img src={arrow} alt="Иконка стрелка" />
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default Portfolio;
