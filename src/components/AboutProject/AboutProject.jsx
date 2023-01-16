ce9118c031b94a145dc1f3e5d0f7e3a979c8f8aa
Sergey Krivenko <Krivenko.Sergei.V@gmail.com>
2023-01-16 18:54:50 +0300

    styles: refactoring #1

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -1,18 +1,18 @@
 function AboutProject() {
   return (
     <section className="project">
-      <h2 className="project__title text_large-secondary underline-pb25">О проекте</h2>
+      <h2 className="project__title project__underline">О проекте</h2>
       <ul className="project__about">
         <li>
-          <h3 className="project__about-title text_large-secondary">Дипломный проект включал 5 этапов</h3>
-          <p className="project__about-text text_medium-secondary">
+          <h3 className="project__about-title">Дипломный проект включал 5 этапов</h3>
+          <p className="project__about-text">
             Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные
             доработки.
           </p>
         </li>
         <li>
-          <h3 className="project__about-title text_large-secondary">На выполнение диплома ушло 5 недель</h3>
-          <p className="project__about-text text_medium-secondary">
+          <h3 className="project__about-title">На выполнение диплома ушло 5 недель</h3>
+          <p className="project__about-text">
             У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы
             успешно защититься.
           </p>
@@ -20,12 +20,12 @@ function AboutProject() {
       </ul>
       <div className="project__duration">
         <div className="project__duration-area">
-          <div className="project__duration-time text_medium-secondary color_primary">1 неделя</div>
-          <p className="project__duration-name text color_text">Back-end</p>
+          <div className="project__duration-time-back">1 неделя</div>
+          <p className="project__duration-name">Back-end</p>
         </div>
         <div className="project__duration-area">
-          <div className="project__duration-time text_medium-secondary color_secondary">4 недели</div>
-          <p className="project__duration-name text_medium-secondary color_text">Front-end</p>
+          <div className="project__duration-time-front">4 недели</div>
+          <p className="project__duration-name">Front-end</p>
         </div>
       </div>
     </section>
--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,14 +1,14 @@
 function Promo() {
   return (
-    <section className="promo color_background">
-      <h1 className="promo__title text_large-primary">
+    <section className="promo">
+      <h1 className="promo__title">
         Учебный проект студента факультета Веб&#8209;разработки.
       </h1>
       <nav>
         <ul className="promo__list">
-          <li className="promo__list-item color_secondary text_medium-primary">О проекте</li>
-          <li className="promo__list-item color_secondary text_medium-primary">Технологии</li>
-          <li className="promo__list-item color_secondary text_medium-primary">Студент</li>
+          <li className="promo__list-item">О проекте</li>
+          <li className="promo__list-item">Технологии</li>
+          <li className="promo__list-item">Студент</li>
         </ul>
       </nav>
     </section>
